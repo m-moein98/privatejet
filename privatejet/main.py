337e4de5b6c29139b1ae41fe53ad30da397f0a64
m-moein98 <moein1475963.mmz@gmail.com>
2023-04-06 23:47:42 +0330

    add request type handler to JetRouter

--- privatejet/main.py
+++ privatejet/main.py
@@ -1,18 +1,57 @@
+import json
+
+
 class PrivateJet:
     routers = []
 
-    async def start(self, scope, send):
+    async def read_body(self, receive):
+        """
+        Read and return the entire body from an incoming ASGI message.
+        """
+        body = b''
+        more_body = True
+
+        while more_body:
+            message = await receive()
+            body += message.get('body', b'')
+            more_body = message.get('more_body', False)
+
+        return json.loads(body)
+
+    async def start(self, receive, scope, send):
         self.scope = scope
-        self.send = send
         assert scope["type"] == "http"
         route = scope["path"]
         for router in self.routers:
             if str(route).startswith(router["prefix"]):
-                router_class = router["router"](send=send)
-                await router_class.handler(route=route[router["prefix"].__len__() :])
+                request = {
+                    "method": scope["method"],
+                    "path": scope["path"],
+                    "query_string": scope["query_string"],
+                    "headers": scope["headers"],
+                    "body": await self.read_body(receive)
+                }
+                match scope["method"]:
+                    case "GET":
+                        if route == router["prefix"] or route == router["prefix"] + "/":
+                            await router["router"](send).get(request)
+                        await router["router"](send).get_one(request)
+                    case "POST":
+                        await router["router"](send).post(request)
+                    case "PUT":
+                        await router["router"](send).put(request)
+                    case "PATCH":
+                        await router["router"](send).patch(request)
+                    case "DELETE":
+                        await router["router"](send).delete(request)
 
     async def add_router(self, router):
         if router["prefix"] not in [
             router_obj["prefix"] for router_obj in self.routers
         ]:
-            self.routers.append(router)
+            self.routers.append(
+                {
+                    "prefix": router["prefix"],
+                    "router": router["router"]
+                }
+            )
